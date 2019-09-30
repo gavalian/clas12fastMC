4032fb9d716d7f9066f62c296cb78f1a2f40086a
Tyler Viducic <tyler.viducic@gmail.com>
2019-09-30 16:20:35 -0400

    Detector class padded out

--- src/main/java/org/jlab/clas12/fastMC/base/Detector.java
+++ src/main/java/org/jlab/clas12/fastMC/base/Detector.java
@@ -5,13 +5,66 @@
  */
 package org.jlab.clas12.fastMC.base;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.jlab.jnp.geom.prim.Path3D;
+import org.jlab.jnp.geom.prim.Point3D;
+import org.jlab.jnp.geom.prim.Shape3D;
 
 /**
  *
  * @author gavalian
+ * @author viducic
  */
 public abstract class Detector {
+    private String name;
+    private double distance;
+    private double tilt;
+    private ArrayList<Shape3D> components = new ArrayList<>();
+
+
     public abstract List<DetectorHit> getHits(Path3D path);
+    public abstract void init();
+
+    public void addComponent(Shape3D shape){
+        this.components.add(shape);
+    }
+
+    public boolean hasIntersection(Path3D path){
+        Iterator<Shape3D> shapes = components.iterator();
+        while(shapes.hasNext()){
+            Shape3D shape = shapes.next();
+            if(shape.hasIntersection(path)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Point3D> intersection(Path3D path, ArrayList<Point3D> points){
+        Iterator<Shape3D> shapes = components.iterator();
+        while(shapes.hasNext()){
+            Shape3D shape = shapes.next();
+            if(shape.hasIntersection(path)){
+                shape.intersection(path, points);
+            }
+        }
+        return points;
+    }
+
+    public int getIntersectionComponent(Path3D path){
+        Iterator<Shape3D> shapes = components.iterator();
+        while(shapes.hasNext()){
+            Shape3D shape = shapes.next();
+            if(shape.hasIntersection(path)){
+                return (this.components.indexOf(shape) + 1);
+            }
+        }
+        return -1;
+    }
+
+    public Shape3D getComponent(int sector){
+        return this.components.get(sector + 1);
+    }
 }
--- src/main/java/org/jlab/clas12/fastMC/detectors/ECDetector.java
+++ src/main/java/org/jlab/clas12/fastMC/detectors/ECDetector.java
@@ -5,17 +5,27 @@
  */
 package org.jlab.clas12.fastMC.detectors;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import org.jlab.clas12.fastMC.base.Detector;
 import org.jlab.clas12.fastMC.base.DetectorHit;
 import org.jlab.jnp.geom.prim.Path3D;
+import org.jlab.jnp.geom.prim.Shape3D;
 
 /**
  *
  * @author gavalian
+ * @authon viducic
  */
 public class ECDetector extends Detector {
+
+    double distance = 721.7;
+    double tilt = 25.0;
+
+    public ECDetector(){
+        initCal();
+    }
 
     @Override
     public List<DetectorHit> getHits(Path3D path) {
