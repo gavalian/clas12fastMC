b519cde48ec59be605af976ed44234be5ea9838c
gavalian <gavalian@meson.jlab.org>
2019-10-22 14:49:50 -0400

    added resolution classes

---
+++ src/main/java/org/jlab/clas12/fastMC/resolution/ProtonResolution.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.clas12.fastMC.resolution;
+
+import org.jlab.jnp.physics.Particle;
+import org.jlab.jnp.physics.ParticleModifier;
+import org.jlab.jnp.physics.PhysicsFactory;
+
+/**
+ *
+ * @author gavalian
+ */
+public class ProtonResolution implements ParticleModifier {
+    
+    private final int pid = 2212;
+    
+    @Override
+    public void modify(Particle part){
+        if(part.pid()==pid){
+            double   pres = getP(part);
+            double   tres = getTheta(part);
+            double phires = getPhi(part);
+            PhysicsFactory.applyResolution(part, pres, phires, phires);
+        }
+    }
+    
+    public int getPid(){
+        return pid;
+    }
+    
+    public double getP(Particle p){
+        return p.p()*0.07;
+    }
+    
+    public double getTheta(Particle p){
+        return 0.02;
+    }
+    
+    public double getPhi(Particle p){
+        return 0.025;
+    }
+}
